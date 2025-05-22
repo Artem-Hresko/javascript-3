844e6f9e2675996d43466fc9b8c728036f5f0533
Artem Hresko <gresko.artem09@gmail.com>
2025-05-22 17:17:01 +0100

    ,

---
+++ js/index.js
@@ -0,0 +1,34 @@
+const result = 5 + 5 + "5";
+console.log(result);
+console.log(typeof result);
+
+const email = "your.email@example.com";
+
+const symbol = email.includes("@");
+
+console.log(email.length);
+
+const word1 = "My";
+const word2 = "name";
+const word3 = "is";
+
+let fullName = word1 + " " + word2 + " " + word3 + " " + "Viktor";
+
+console.log(fullName);
+
+const userName = "Олександро";
+const payment = 300;
+
+alert(
+  "Дякуємо," +
+    " " +
+    userName +
+    " " +
+    "До" +
+    " " +
+    "сплати" +
+    " " +
+    payment +
+    " " +
+    "гривень"
+);
