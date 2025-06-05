a9a36c40d7d0ab8e8967c5fb9118fb4f4357d380
Artem Hresko <gresko.artem09@gmail.com>
2025-06-05 18:37:39 +0100

    a

--- js/index.js
+++ js/index.js
@@ -20,15 +20,5 @@ const userName = "Олександро";
 const payment = 300;
 
 alert(
-  "Дякуємо," +
-    " " +
-    userName +
-    " " +
-    "До" +
-    " " +
-    "сплати" +
-    " " +
-    payment +
-    " " +
-    "гривень"
+  `Дякуємо, ${userName} Досплати ${payment} гривень`
 );
